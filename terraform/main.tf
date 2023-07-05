da793ec321c89b15a435d3d701f03deb6a5e465f
crystallo <crystallo0v0@gmail.com>
2023-07-04 21:31:45 -0700

    typo

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
     region = "${var.aws_region}"
 }
 
-data "aws_caller_identify" "current" {}
+data "aws_caller_identity" "current" {}
 
 locals {
     account_id = data.aws_caller_identity.current.account_id
