aa9f812f93817ba0b1815ea00bc60156a1f05586
crystallo <crystallo0v0@gmail.com>
2023-07-04 21:42:21 -0700

    typo

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -41,7 +41,7 @@ resource "aws_lb_target_group" "target_group" {
         matcher = "200"
         timeout = "3"
         path = "/"
-        unhealthy_threhsold = "2"
+        unhealthy_threshold = "2"
     }
 }
 
